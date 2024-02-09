d754e9c8201183c236b421ff3c6f824ad6a6b888
Yves <yves.steffen@ard.de>
2024-02-09 02:35:03 +0100

    :sparkles:

--- scripts.js
+++ scripts.js
@@ -4,8 +4,8 @@ window.onload = function(){
     document.getElementById("title").innerHTML = "Will you be my Valentine ?";
 };
 
-function changeElement(id, string){
-    document.getElementById(id).innerHTML = string;
+Array.prototype.random = function () {
+    return this[Math.floor((Math.random()*this.length))];
 }
 
 function clickYes(){
@@ -17,3 +17,35 @@ function clickYes(){
     document.getElementById('btns').classList.add('yes')
     document.getElementById('container').classList.add('spin')
 }
+
+let click = 0
+function clickNo(){
+    if (click == 0) {
+        click++
+        document.getElementById("title").innerHTML = "Sure ? :(";
+        document.getElementById("btn n").innerText = "Sure ? :("
+        document.getElementById("output").innerHTML = "Sure ? :(";
+    }
+    else funcs[Math.floor(Math.random() * funcs.length)]();
+}
+
+function drop(){
+    console.log("drop")
+}
+function remove(){
+    console.log("remove")
+    document.getElementById('btn n').disabled = true
+    document.getElementById('btn n').classList.add('hidden')
+}
+
+function hide(){
+    console.log("hide")
+    document.getElementById('btn n').classList.add('hide')
+    document.getElementById('btn n').disabled = true
+}
+
+let funcs = [
+    drop,
+    remove,
+    hide
+]
